8725081fab1e6bd862f940882b0dc97f9d1802f1
kaoutar1987 <kaoutarhabach@gmail.com>
2021-04-08 11:25:26 +0100

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const connectDB =require('./database/db');
 const cors = require("cors");
 require("dotenv").config();
 
@@ -9,6 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
+
+
+connectDB();
+app.get('/',(req ,res)=>{
+    res.send('Inside Server')
+})
 const port = 5000;
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
